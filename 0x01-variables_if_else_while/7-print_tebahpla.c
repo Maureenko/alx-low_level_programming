bf8868ba6b349f329f202fd767556135f09e48b9
Maureen Rotich <mautich06@gmail.com>
2023-09-08 12:14:08 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main code here
+ *
+ * Return: is 0
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}

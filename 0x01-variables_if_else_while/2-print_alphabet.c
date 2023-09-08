5de3773a9a9bfaebcf8868a063d3fd7e22b37f09
Maureen Rotich <mautich06@gmail.com>
2023-09-08 09:39:54 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - imortant code here
+ *
+ * Return: is 0 after execution
+ */
+int main(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+	putchar(i);
+	putchar('\n');
+	i++;
+	}
+	return (0);
+}

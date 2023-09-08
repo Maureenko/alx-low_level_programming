7eb47581bad2703d411dbf5591f369ae81bed4b1
Maureen Rotich <mautich06@gmail.com>
2023-09-08 13:31:54 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,16 +10,10 @@ int main(void)
 	int i;
 	char c;
 
-	for (i = 1; i <= 16; i++)
-	{
-	if (i <= 10)
-	putchar(i);
-	else
-	{
+	for (i = 0; i <= 9; i++)
+	putchar ('0' + i);
 	for (c = 'a'; c <= 'f'; c++)
-	putchar(c);
-	}
-	}
+	putchar (c);
 	putchar ('\n');
 	return (0);
 }

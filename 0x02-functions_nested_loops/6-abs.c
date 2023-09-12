6fd2f102aef1ecba901bcbef3d21f269accf1b88
Maureen Rotich <mautich06@gmail.com>
2023-09-12 15:07:34 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value of a number
+ *
+ * Return: always 0
+ */
+int _abs(int j)
+{
+	if (j < 0)
+	return (-j);
+	else
+	return (j);
+
+	return (0);
+}

b9b661b924f168ce883d7bebe5105f6a822a55fa
Maureen Rotich <mautich06@gmail.com>
2023-09-12 18:26:54 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,27 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-		_putchar(i * j + '0');
-		_putchar(',');
-		_putchar(' ');
-		}
+	for (j = 0; j <= 9; j++)
+	{
+	int result = i * j;
+
+	if (result < 10)
+	{
+	_putchar(' ');
+	_putchar(result + '0');
+	}
+	else
+	{
+	_putchar(result / 10 + '0');
+	_putchar(result % 10 + '0');
+	}
+	if (j < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
 	_putchar('\n');
 	}
 }
+

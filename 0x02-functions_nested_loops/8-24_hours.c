98d75ac5c87281fbb9b6cd86d4c422f540982c2f
Maureen Rotich <mautich06@gmail.com>
2023-09-12 17:46:42 -0700

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,21 +5,22 @@
  */
 void jack_bauer(void)
 {
-	int hour, min;
+	int hour;
+	int min;
 
 	hour = 0;
 	while (hour < 24)
 	{
-	min = 0;
-	while (min < 60)
-	{
-	 _putchar(hour / 10 + '0');
-	 _putchar(hour % 10 + '0');
-	_putchar( ':');
-	_putchar(min / 10 + '0');
-	_putchar(min % 10 + '0');
-	_putchar('\n');
-	min++;
+		min = 0;
+		while (min < 60)
+		{
+		_putchar(hour / 10 + '0');
+		_putchar(hour % 10 + '0');
+		_putchar(':');
+		_putchar(min / 10 + '0');
+		_putchar(min % 10 + '0');
+		_putchar('\n');
+		min++;
 	}
 	hour++;
 	}

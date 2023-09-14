c9b4196112ab42eba3312d896d975ec3143d61bb
Maureen Rotich <mautich06@gmail.com>
2023-09-14 10:00:32 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,14 +9,8 @@
  */
 int _isdigit(int c)
 {
-	int i;
-
-	for (i = 0; i <= 127; i++)
-	{
-		if (c >= 0 && c <= 9)
-			return (1);
-		else
-			return (0);
-	}
-	return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }

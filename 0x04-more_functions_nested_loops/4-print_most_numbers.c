294d1835c85a884bb8349c707220951c6edf0f6c
Maureen Rotich <mautich06@gmail.com>
2023-09-14 12:15:20 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - rint numbers 0-9 without 2 and 4
+ *
+ * Return: always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		if(i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}

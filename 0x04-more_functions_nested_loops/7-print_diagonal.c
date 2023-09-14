bdbe824061ff52ad145994ed73178e36b2593d0b
Maureen Rotich <mautich06@gmail.com>
2023-09-14 14:14:33 -0700

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,20 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j <= i; j++)
 		{
-			if (n > 0 && n != 0)
-			{
-				if (j == i)
-					_putchar('\\');
-				else
-					_putchar(' ');
-			}
+			if (j == i)
+				_putchar('\\');
+			else
+				_putchar(' ');
 		}
 		_putchar('\n');
+	}
 	}
 }

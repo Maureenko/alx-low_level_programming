6aac00e5a27bbfcdf394ca65a250e5add3730f8e
Maureen Rotich <mautich06@gmail.com>
2023-09-14 18:13:36 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if(i % 15 == 0)
-			printf("FizzBuzz ");
+		if (i % 15 == 0)
+			printf("%s ", "FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("%s ", "Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("%s ", "Fizz");
 		else
 			printf("%d ", i);
 	}

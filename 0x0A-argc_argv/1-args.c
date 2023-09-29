ba4127be175e9c51be9f53ca4a9d665ac11383fc
Maureen Rotich <mautich06@gmail.com>
2023-09-29 01:56:32 -0700

    print args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0;
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

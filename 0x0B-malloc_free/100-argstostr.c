14134d6c3118b254564cebefe6a0d7a1d74c1cbb
Maureen Rotich <mautich06@gmail.com>
2023-10-04 16:29:41 -0700

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,6 +23,8 @@ char *argstostr(int ac, char **av)
 	p = malloc(sizeof(char) * len + 1);
 	if (p == NULL)
 		return (NULL);
+	if (ac == 0)
+		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)

bb96be498da4b37fc8a5ee96f3b304e200647e0f
Maureen Rotich <mautich06@gmail.com>
2023-10-04 04:56:30 -0700

    0-create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	str = (char *)malloc(size * sizeof(char));
-	for (i = 0; i < size; i++)
-	str[i] = c;
 	if (size == 0)
 		return (NULL);
 	else if (str == NULL)
 		return (NULL);
-	else
-		return (str);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }

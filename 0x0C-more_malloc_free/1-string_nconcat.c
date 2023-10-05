231ecd5e8ad145025797becc09663923d9f7bc40
Maureen Rotich <mautich06@gmail.com>
2023-10-05 09:44:17 -0700

    2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			p[i] = s1[i];
-		for (i = 0; s2[i] != '\0'; i++)
+		for (i = 0; i < n; i++)
 			p[len1 + i] = s2[i];
 		p[len1 + i] =  '\0';
 	}

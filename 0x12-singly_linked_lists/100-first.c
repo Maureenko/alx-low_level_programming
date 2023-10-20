3110752dfef1d2bd68ebe482bedb4df9d3bcd05f
Maureen Rotich <mautich06@gmail.com>
2023-10-20 13:47:56 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first_print - prints message
+ */
+void first_print(void)
+{
+	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+}

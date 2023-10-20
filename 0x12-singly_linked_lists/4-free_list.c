6e1823289891c7534bea9aeecd968326ecf36140
Maureen Rotich <mautich06@gmail.com>
2023-10-20 08:42:23 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list
+ * @head: pointer to head of string
+ */
+void free_list(list_t *head)
+{
+	list_t *node0;
+
+	while (head != NULL)
+	{
+	node0 = head;
+	if (node0->str != NULL)
+		free(node0->str);
+	free(node0);
+	}
+}

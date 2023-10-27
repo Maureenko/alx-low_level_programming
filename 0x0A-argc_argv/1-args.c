986ffc4705849835e4b0e3ebab6d65ce7b78b4a3
Maureen Rotich <mautich06@gmail.com>
2023-10-27 04:36:18 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print no of arguments passed
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: always 0
+ */
+int main(int argc, __attribute__((unused))char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}

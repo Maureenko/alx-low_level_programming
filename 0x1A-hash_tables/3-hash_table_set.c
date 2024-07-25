44ee06e19341fdb65d1e79b926970b9d412c3f20
Maureen Rotich <mautich06@gmail.com>
2024-07-25 10:14:15 +0300

    adding elements to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds elements to a hash table
+ * @ht: hashtable to added or udated to key/value pair
+ * @key: key
+ * @value: value associated with the key
+ * Return: 1 on success, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new = NULL;
+	unsigned long int index;
+
+	if (*key == '\0' || value == NULL || ht == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new = malloc(sizeof(hash_node_t));
+
+	if (new == NULL)
+	return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
+
